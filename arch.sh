091b1c61a491cab65d3e499617f3244a6c50832d
Krupass <jindrasima11@seznam.cz>
2024-04-20 12:32:33 +0200

    First try V10

--- arch.sh
+++ arch.sh
@@ -5,18 +5,20 @@ echo "This is the best Arch Linux installation script, behold my ultimate power!
 echo "Creating sda disk"
 
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
-  g
-  n # new partition
-  1 # partition number 1
-    # default - start at beginning of disk 
-  +1G # 1 GB boot parttion
+  g	# new GPT table
+  n 	# new partition
+  1 	# partition number 1
+   	# default
+  +1G 	# partition size
+  t 	# change type
+  1 	# EFI
   
-  n # new partition
-  2 # partion number 2
-    # default, start immediately after preceding partition
-  +10G #
-
-  p # print the in-memory partition table
+  n 	# new partition
+  2 	# partion number 2
+    	# default
+  +10G 	# partition size
+	# default
+  p 	# print the in-memory partition table
 EOF
 
 
