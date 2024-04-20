eac245d0069cbb0d1b8e127db3c16e1d3b796a93
Krupass <jindrasima11@seznam.cz>
2024-04-20 12:07:40 +0200

    First try V4

--- arch.sh
+++ arch.sh
@@ -6,16 +6,18 @@ echo "Creating sda disk"
 
 fdisk /dev/sda
 echo "new GPT table"
-g
+echo "g"
 echo "New partition"
-n
+echo "n"
 echo "Partition number"
-1
+echo "1"
 echo "First sector"
 echo -ne '\n'
 echo "Last sector"
-+1G
+echo "+1G"
 echo "Partition type"
-ef00
+echo "ef00"
 echo "Result"
-p
+echo "p"
+
+
